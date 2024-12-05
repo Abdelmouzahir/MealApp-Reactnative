7da70748f130275fb3e2c6cd65a81654b38bb1d7
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-12-05 18:33:17 -0500

    add notification

--- components/List.js
+++ components/List.js
@@ -1,28 +1,26 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
 
-
-export default function List({data}) {
-    return  data.map((dataPoint) => (
-        <View style={styles.listItem}>
-          <Text style={styles.listText} key={dataPoint}>{dataPoint}</Text>
-        </View>
-    ));
+export default function List({ data }) {
+  return data.map((dataPoint) => (
+    <View style={styles.listItem}>
+      <Text style={styles.listText} key={dataPoint}>
+        {dataPoint}
+      </Text>
+    </View>
+  ));
 }
 
-
 const styles = StyleSheet.create({
-    listText:{
-       color: 'white',
-       textAlign: 'center',
-       
-    },
-    listItem:{
-        borderRadius: 6,
-        paddingHorizontal: 8,
-        paddingVertical: 4,
-        marginVertical: 4,
-        marginHorizontal:12,
-        backgroundColor: '#D04848'
-    }
+  listText: {
+    color: "white",
+    textAlign: "center",
+  },
+  listItem: {
+    borderRadius: 6,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    marginVertical: 4,
+    marginHorizontal: 12,
+    backgroundColor: "#D04848",
+  },
 });
-
--- components/MealItem.js
+++ components/MealItem.js
@@ -1,59 +1,113 @@
-import { View, Text, Pressable, Image, StyleSheet, Platform} from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  Image,
+  StyleSheet,
+  Platform,
+  Button,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import MealDetails from "./MealDetails";
+import React, { useEffect } from "react";
+import * as Notifications from "expo-notifications";
 
-export default function MealItem({ title, imageUrl, duration , complexity, affordability, id}) {
-    const navigation = useNavigation();
+export default function MealItem({
+  title,
+  imageUrl,
+  duration,
+  complexity,
+  affordability,
+  id,
+}) {
+  const navigation = useNavigation();
 
-    function navigateToDetailScreen() {
-        navigation.navigate('DetailScreen' , {mealId :id});
-    }
+  Notifications.setNotificationHandler({
+    handleNotification: async () => {
+      return {
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      };
+    },
+  });
+
+  function navigateToDetailScreen() {
+    navigation.navigate("DetailScreen", { mealId: id });
+  }
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log(notification);
+      }
+    );
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  function sendNotification() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: title,
+        body: "This meal has been added to your favorites",
+      },
+      trigger: {
+        seconds: 2,
+      },
+    });
+  }
 
   return (
     <View style={styles.mealItem}>
-      <Pressable android_ripple={{color: "#ccc"}}
-       style={({pressed}) =>  pressed ? {opacity: 0.5} : null}
-       onPress={navigateToDetailScreen}
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? { opacity: 0.5 } : null)}
+        onPress={navigateToDetailScreen}
       >
-      <View  style={styles.innerContainer}>
-        <View>
-        <Image source={{uri: imageUrl}}   style={styles.image} />
-        <Text style={styles.title}>{title}</Text>
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <MealDetails
+            duration={duration}
+            complexity={complexity}
+            affordability={affordability}
+          />
         </View>
-        <MealDetails  duration={duration} complexity={complexity}  affordability={affordability}/>
-       </View> 
       </Pressable>
+      <Button title="View Details" onPress={sendNotification} />
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
-    innerContainer:{
-        borderRadius: 8,
-        overflow: "hidden"
-    },
-    mealItem:{
-        margin:16,
-        borderRadius: 8,
-        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
-        backgroundColor: 'white',
-        elevation:4,
-        //ios
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 8,
-        shadowOpacity: 0.25,
-    },
-    image: {
-        width: '100%',
-        height: 200,
-    },
-    title:{
-        fontWeight: 'bold',
-        fontSize: 18,
-        textAlign: 'center',
-        margin:8
-    },
-    
-})  
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    backgroundColor: "white",
+    elevation: 4,
+    //ios
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    shadowOpacity: 0.25,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
+    margin: 8,
+  },
+});
